cf4389a0a889ad9d8c209fd6283d3fd724c6650b
thonpair <emmanuel@hemer.it>
2021-03-11 10:05:33 +0100

    tests ok, display not ok

--- main.js
+++ main.js
@@ -16,7 +16,14 @@ fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-ga
   const width = 900,
   height = 400,
   marginWidth = 60,
-  marginHeight = 50;
+  marginHeight = 50,
+  marginCell = 1;
+
+  const colors = [
+    "#3393ff", //blue
+    "#d2ff4f", //yellow
+]
+
 
 let tooltip = d3
   .select('#app')
@@ -31,10 +38,76 @@ let svgContainer = d3
   .attr('height', height + marginHeight)
   .attr('class', "svgContainer");
 console.log(data)
-  svgContainer.selectAll('rect')
+let xNext = 0;
+  let tileContainer = svgContainer.selectAll('g')
   .data(data.children)
+  .enter()
+  .append('g')
+  .attr('class', 'tileContainer')
+  .selectAll('rect')
+  .data(d => d.children)
   .enter()
   .append('rect')
   .attr('class', 'tile')
+  .attr('data-name', d => d.name)
+  .attr('data-category', d => d.category)
+  .attr('data-value', d => d.value)
+  .attr('fill', d => d.value > 30 ? colors[0]: colors[1])
+  .attr('x', (d) => {
+    let xCurrent = xNext
+    xNext+=parseInt(d.value)+marginCell
+    return xCurrent
+  })
+  .attr('y', 0)
+  .attr('height', d => parseInt(d.value))
+  .attr('width', d=> parseInt(d.value))
+  .on('mouseover', (d, i) => {
+    tooltip
+        .html("value : " + i.value + "<br/>"
+        )
+        .attr('all', JSON.stringify(i))
+        .attr('data-value', i.value)
+        .style('left', d.clientX + 'px')
+        .style('top', height - 100 + 'px')
+        .style('transform', 'translate('+d.clientX+'px, '+d.clientY+'px)')
+        .style('opacity', 0.9);
+})
+.on('mouseout', function () {
+  tooltip.style('opacity', 0);
+});
+
+
+
+
+
+
+
+
+  const legend = d3.select("#app")
+  .append('div')
+  .attr('id', 'legend')
+  .text('Legend : ')
+
+  const svglegend = legend
+  .append('svg')
+  .attr("height", 25)
+  .attr("width", 300)
+
+
+
+  svglegend
+  .selectAll('rect')
+  .data(colors)
+  .enter()
+  .append("rect")
+  .attr("x", (d,i) => i*25)
+  .attr("y", 0)
+  .attr("width", 25)
+  .attr("height", 25)
+  .attr("fill", d =>d)
+  .attr("class","legend-item" )
+
+
+
 
 })
